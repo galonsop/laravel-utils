a281ec67c1f7f0663219a41966ce80c57811332c
Gap <g.alonso.per@gmail.com>
2023-06-02 19:17:35 +0200

    fix: remove <strong> from select multiple

--- resources/views/partials/forms/select-multiple.blade.php
+++ resources/views/partials/forms/select-multiple.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    <label class="form-label" for="{{ $name }}"><strong>{{ $label }}</strong>{{ (!isset($required) || $required) ? '*' : ''}}</label>
+    <label class="form-label" for="{{ $name }}">{{ $label }}{{ (!isset($required) || $required) ? '*' : ''}}</label>
     <select class="form-control {{ $class ?? '' }}" name="{{ $name }}[]" {{ (!isset($required) || $required) ? 'required' : '' }} multiple data-live-search="true" data-old="{{ implode(',',old($name, $value)) }}">
         @foreach($options as $val => $label)
             <option value="{{ $val }}" {{ is_array(old($name, $value)) && in_array($val, old($name, $value)) ? 'selected' : '' }} >{{ $label }}</option>
